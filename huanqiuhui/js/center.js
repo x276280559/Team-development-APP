bce90ebaa44a598e18f2f9c49010ba698dca1b3d
chenh520 <193851008@qq.com>
2018-05-16 17:21:41 +0800

    ..

--- huanqiuhui/js/center.js
+++ huanqiuhui/js/center.js
@@ -4,8 +4,8 @@
 			$('.mui-scroll-wrapper').scroll({
 				indicators: true //是否显示滚动条
 			});
-			var html2 = '<ul class="mui-table-view"><li class="mui-table-view-cell"><span class="mui-icon mui-icon-arrowright"></span>购买价格</li><li class="mui-table-view-cell">购买AEQ数量:*<span>必填</span></li><li class="mui-table-view-cell" id="hao-li2">支付方式<span class="mui-icon mui-icon-arrowright"></span><span>金积分</span></li></ul>';
-			var html3 = '<ul class="mui-table-view"><li class="mui-table-view-cell">持有AEQ数量</li><li class="mui-table-view-cell"><span class="mui-icon mui-icon-arrowright"></span>出售价格</li><li class="mui-table-view-cell">出售AEQ数量:*<span>必填</span></li><li class="mui-table-view-cell" id="hao-li3">税费<span>0%</span></li></ul>';
+			var html2 = '<ul class="mui-table-view"><li class="mui-table-view-cell"><span class="mui-icon mui-icon-arrowright"></span>购买价格<input type="text" id="hao-inp" placeholder="购买价格"/></li><li class="mui-table-view-cell"><p style="clear: left;">购买AEQ数量：*<span id="block-range-val">1必填<h5>(最大选择数量为1)</h5></span></p><div class="mui-input-row mui-input-range"><input type="range" id="block-range" value="1" min="0" max="1"></div></li><li class="mui-table-view-cell" id="hao-li2">支付方式<span class="mui-icon mui-icon-arrowright"></span><span>金积分</span></li></ul>';
+			var html3 = '<ul class="mui-table-view"><li class="mui-table-view-cell">持有AEQ数量</li><li class="mui-table-view-cell"><span class="mui-icon mui-icon-arrowright"></span>出售价格<input type="text" id="hao-inp" placeholder="出售价格"/></li><li class="mui-table-view-cell"><p style="clear: left;">出售AEQ数量：*<span id="block-range-val">1必填<h5>(最大选择数量为1)</h5></span></p><div class="mui-input-row mui-input-range"><input type="range" id="block-range" value="1" min="0" max="1"></div></li><li class="mui-table-view-cell" id="hao-li3">税费<span>0%</span></li></ul>';
 			var item2 = document.getElementById('item2mobile');
 			var item3 = document.getElementById('item3mobile');
 			document.getElementById('slider').addEventListener('slide', function(e) {
@@ -39,4 +39,16 @@
 	phone.addEventListener("tap",function(){
 		phone.href="PrePaid-phone.html"
 	})
+		 var rangeList = document.querySelectorAll('input[type="range"]');
+	    for(var i=0,len=rangeList.length;i<len;i++){
+	        rangeList[i].addEventListener('input',function(){
+	            if(this.id.indexOf('field')>=0){
+	                document.getElementById(this.id+'-input').value = this.value;
+	                
+	            }else{
+	                document.getElementById(this.id+'-val').innerHTML = this.value;
+	            }
+	            
 	
+	        });
+	    }
